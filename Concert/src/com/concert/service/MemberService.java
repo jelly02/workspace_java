d9299c6c2bfb34782cf6c70f1c7222eee2bec590
jelly02 <haribo__@naver.com>
2021-06-08 23:23:28 +0900

    새로 만든 concert 추가

---
+++ Concert/src/com/concert/service/MemberService.java
@@ -0,0 +1,332 @@
+package com.concert.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.concert.model.Info;
+import com.concert.model.Member;
+//import com.work.exception.CommonException;
+import com.concert.model.Reservation;
+import  com.concert.view.MainMenu;
+
+/**
+ * 서비스 클래스 
+ * 
+ * @author KIMJURI
+ *
+ */
+
+
+public class MemberService {
+	
+	/** 예약 도메인 클래스 */
+	public Reservation res = new Reservation();
+
+	
+	
+	/**  회원들을 저장하기 위한 자료 저장 구조 */
+	public ArrayList<Member> list = new ArrayList<Member>();
+	/** 공연 정보 저장 리스트 */
+	public ArrayList<Info> play = new ArrayList<Info>();
+	
+	/** 공연 좌석  */
+	String[] vipSeat = new String[]{"1","2","3","4","5","6","7","8","9","10"} ;
+	String[] sSeat = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+	String[] rSeat = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+	
+	/** 예약 정보 저장 */
+	public ArrayList<Reservation> reservation = new ArrayList<Reservation> ();
+	
+	
+	
+	/** 기본 생성자 */
+	public MemberService() {	
+		
+		// 회원 초기화 
+		list.add(new  Member("admin","admin1234","관리자","01082828282","A",0));
+		list.add(new  Member("user01","user01","김주리","01011111111","A",0));
+		
+		//공연 정보 저장 		
+		play.add(new Info("play_1", "뮤지컬 더 리퍼", "6/12", "19:30 ~ 22:00", "문채원", "송중기"));
+		play.add(new Info("play_2", "옥탑방 고양이", "6/19", "20:00 ~ 22:00", "남정은", "이경민"));
+		play.add(new Info("play_3", "피카츄의 대모험", "6/26", "19:30 ~ 21:30", "소니아", "금랑"));
+		play.add(new Info("play_4", "낫아웃", "7/3", "20:00 ~ 22:30", "송이재", "정재광"));
+		play.add(new Info("play_5", "행복", "7/10", "19:30 ~ 21:30", "김단율", "정영성"));
+
+	//	refresh();
+		}
+		
+	
+	
+	/** 현재 등록한 전체 회원 조회 */
+	public ArrayList<Member> getMember() {
+		
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		return list;
+	}
+	
+	
+	/**
+	 * 로그인
+	 * @param memberId
+	 * @param memberPw
+	 * @return 성공시 true, 실패시 false
+	 * @throws CommonException 
+	 */
+	public boolean login(String memberId, String memberPw)  {
+		
+		int exist = exist(memberId);
+		Member dto = getMember(memberId);
+		
+		if (exist != -1 && dto.getMemberPw().equals(memberPw)  ) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+
+	/**
+	 * 회원 존재 유무 조회 메서드
+	 * @param memberId 아이디
+	 * @return 존재 시 저장 위치 인덱스 번호, 미존재시 -1
+	 */
+	private int exist(String memberId) {
+
+		for (int index = 0; index < list.size(); index++) {
+			if(list.get(index).getMemberId().equals(memberId)) {
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	
+	/**
+	 *  회원 등록 메서드
+	 * @param member
+	 */
+	public boolean addMember(String memberId, String memberPw, String name, String mobile) {
+			
+		int index = exist(memberId);
+		
+		if(index >= 0 ) {
+			System.out.println("[오류] 해당 아이디는 이미 존재하는 아이디 입니다.");
+			System.out.println("[오류] 회원가입이 정상적으로 처리 되지 않았습니다.");
+			
+			return false;
+		}else {
+			list.add(new Member(memberId, memberPw, name, mobile,"G",0));
+			System.out.println("[안내] : "+name+"님의 회원 가입이 정상적으로 처리 됐습니다.");
+			return true;
+		}
+		
+	}	
+	/**
+	 * 회원 상세조회
+	 * @param memberId 회원아이디
+	 * @return 존재하면 회원객체, 미존재 null
+	 * @throws RecordNotFoundException 
+	 * @see exist(String)
+	 */
+	public Member getMember(String memberId){
+		int index = exist(memberId);
+		if (index >= 0) {
+			return (Member)list.get(index);
+		}
+		
+		return null;
+	}
+
+	/**
+	 *  공연 정보 조회
+	 */
+	public ArrayList<Info> getPlayPrint() {
+		
+		for(int i=0; i<play.size(); i++) {
+			System.out.print((i)+1+" : ");
+			System.out.print(play.get(i).getInfoName() +" / ");
+			System.out.print(play.get(i).getInfoDate()+" / ");
+			System.out.print(play.get(i).getInfoTime()+" / ");
+			System.out.print(play.get(i).getInfoFemaleActor()+" / ");
+			System.out.print(play.get(i).getInfoMaleActor()+" / ");
+			
+			System.out.println();
+		}
+		
+		return play;
+	}
+	
+	/**
+	 *  공연 예약 하기 
+	 *  
+	 */
+	
+	public boolean reservation(int playNum) {
+		
+		String editCode = "play_"+playNum;
+		
+		int index = playExist(editCode);
+		
+		//해당 공연이 존재하면 
+		if(index>= 0) {
+			 seatReservation(editCode);
+			 
+			 return true;
+		}else {
+			//사용자가 딴 값을 입력하면 
+			System.out.println("[오류] 해당 번호의 공연은 존재하지 않습니다. 화면에 나와있는 공연 숫자를 적어주세요");
+			return false;
+		}
+	
+		
+	}
+
+
+	/**
+	 * 공연 존재 유무 조회 메서드
+	 * @param memberId 아이디
+	 * @return 존재 시 저장 위치 인덱스 번호, 미존재시 -1
+	 */
+	public int playExist(String editCode) {
+	
+		for (int index = 0; index < play.size(); index++) {
+			if(play.get(index).getInfoCode().equals(editCode)) {
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 *  공연 좌석 전체 출력 메소드
+	 */
+	
+	public void seatReservation(String editCode) {
+		
+			System.out.print("VIP석 : ");
+				for(int i=0; i<vipSeat.length; i++) {
+		            System.out.print("[ "+vipSeat[i] + " ]");
+		        }
+				System.out.println();
+			
+			System.out.print("S석 : ");
+				for(int i=0; i<sSeat.length; i++) {
+		            System.out.print("[ "+sSeat[i]+ " ]");
+		        }
+				System.out.println();
+			System.out.print("R석 : ");
+				for(int i=0; i<rSeat.length; i++) {
+		            System.out.print("[ "+rSeat[i]+ " ]");  
+				}
+				System.out.println();
+	}
+	
+	/**
+	 *  사용자가 원하는 좌석만 보여주는 메서드 
+	 */
+	
+	public boolean userSeatChoice(int type) {
+		
+		switch(type) {
+		
+			case 1 : System.out.print("VIP석 : ");
+				for(int i=0; i<vipSeat.length; i++) {
+		            System.out.print("[ "+ vipSeat[i] + " ]");
+		        }
+				System.out.println();
+				return true;
+			
+			case 2 : System.out.print("S석 : ");
+				for(int i=0; i<sSeat.length; i++) {
+		            System.out.print("[ "+sSeat[i] + " ]");
+		        }
+				System.out.println();
+				return true;
+				
+			case 3 :System.out.print("R석 : ");
+				for(int i=0; i<rSeat.length; i++) {
+		            System.out.print("[ "+rSeat[i]+ " ]");  
+				}
+				System.out.println();
+				return true;
+			
+			default : 
+				//1~3 사이의 숫자 입력 안했을 경우
+                System.out.println("[오류] 잘못 입력하였습니다. 다시 입력해주세요.");
+                return false;
+		}
+}
+	
+	/**
+	 *  좌석 예약
+	 */
+	
+	public boolean userSeatChoice(int playCode, int type, int seatChoice, String memberId ) {
+		
+		String modifyCode = "play_"+ playCode;
+		String seat = "";
+		
+
+		if(type == 1) {
+			seat = "VIP석";
+		}else if(type == 2 ) {
+			seat = "S석";
+		}else {
+			seat = "R석";
+		}
+		//System.out.println("userSeatChoice 리스트에 넣기 전 "+modifyCode+seat+seatChoice+memberId );
+		
+		try {
+			switch(type) {
+			
+				case 1 :
+						
+						vipSeat[seatChoice-1] = "X";
+						reservation.add(new Reservation(memberId, modifyCode, seat, seatChoice));
+						getReservationList();
+						return true;
+						
+				case 2 : 
+					
+						sSeat[seatChoice-1] = "X";
+						reservation.add(new Reservation(memberId, modifyCode, seat, seatChoice));
+						getReservationList();
+						return true;
+					
+				case 3 :
+						rSeat[seatChoice-1] = "X";
+						reservation.add(new Reservation(memberId, modifyCode, seat, seatChoice));
+						getReservationList();
+						return true;
+			}
+		}catch (Exception e) {
+			System.out.println("[오류] 정확한 좌석 번호를 다시 입력해주세요.");
+		}
+		
+		
+		
+		
+		return false;
+		
+	}
+	
+		/**
+		 *  공연 정보 조회
+		 */
+		public ArrayList<Reservation> getReservationList() {
+			
+			for (int i = 0; i < reservation.size(); i++) {
+			    System.out.print(reservation.get(i) + " ");
+			}
+			
+			return reservation;
+	}
+	
+
+	
+	
+}
---
+++ Concert/src/com/concert/view/MainMenu.java
@@ -0,0 +1,345 @@
+package com.concert.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.concert.model.Reservation;
+import com.concert.service.MemberService;
+
+
+public class MainMenu {
+
+
+	/** 회원관리 서비스 클래스 */
+	public MemberService service = new MemberService();
+	
+	/** 예약 정보 저장 */
+	public ArrayList<Reservation> reservation = new ArrayList<Reservation> ();
+	
+	
+
+	/** 메인 화면 */
+	public void mainMenu() {
+		
+		printLine();
+		System.out.println(" \t00 공연 관리 프로그램");
+		System.out.println();
+		
+		printSpace("1. 로그인");
+		printSpace("2. 회원가입");
+		printSpace("3. 프로그램 종료");
+	
+		System.out.println();
+		printLine();
+		
+		System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
+		int menuNo = inputNumber();
+		
+		switch(menuNo) {
+		
+		case 1:
+			System.out.println("1. 로그인");
+			loginMenu();
+			break;
+		case 2:
+			System.out.println("2. 회원가입");
+			addMemberMenu();
+			break;
+		case 3:
+			System.out.println("3. 프로그램 종료");
+			System.exit(0);;
+			break;
+		default:
+			System.out.println("[오류] 화면에 표시된 숫자만 입력해주세요.");
+			break;	
+		
+		}
+		
+	}
+
+	/**
+	 *  로그인 
+	 */
+
+	public void loginMenu() {
+
+			
+		printLine();
+		System.out.println(" \t\t로그인");
+		System.out.println();
+				
+		System.out.print("아이디 : ");
+		String memberId = inputString();
+				
+		System.out.print("비밀번호 : ");
+		String memberPw = inputString();
+				
+		boolean result = service.login(memberId, memberPw);
+		
+		
+		if(result) {
+			System.out.println();
+			System.out.println("[안내] 로그인 성공! "+memberId+"님 환영합니다!");
+			System.out.println();
+			
+			// 로그인한 아이디를 예약하는 곳에 뿌려줘야 예약과 조회가 되겠다!
+			memberMenu(memberId);
+			
+		}else if(result == false){
+			System.out.println("[오류] 입력한 정보와 맞는 회원이 없습니다. 다시 시도해주세요.");
+			mainMenu();
+		}	
+	}
+	
+	/**
+	 *  회원 가입
+	 */
+	public void addMemberMenu() {
+		
+		printLine();
+		System.out.println(" \t\t회원가입");
+		System.out.println();
+		
+		System.out.println("아이디 : ");
+		String memberId = inputString();
+		
+		System.out.println("비밀번호 : ");
+		String memberPw = inputString();
+		
+		System.out.println("이름: ");
+		String name = inputString();
+		
+		System.out.println("휴대폰 : ");
+		String mobile = inputString();
+		
+		boolean result = service.addMember(memberId, memberPw, name, mobile);
+
+		if(result) {
+			mainMenu();
+		}else {
+			mainMenu();
+		}
+		
+	}
+
+	public void memberMenu(String memberId) {
+		printLine();
+
+		printSpace("00 공연 관리 프로그램");
+		System.out.println();
+		
+		printSpace("1. 공연 정보 조회 ");
+		printSpace("2. 공연 예약");
+		printSpace("3. 공연 예약 조회");
+		printSpace("0. 로그아웃");
+		System.out.println();
+		printLine(); 
+		
+		int menuNo = inputNumber();
+		
+		switch(menuNo) {
+		
+		//공연 정보 조회
+		case 1:
+			infoCheck(memberId); 
+			break;
+		
+		//공연 예약
+		case 2:
+			infoReservation(memberId);
+			break;
+		
+		
+		//예약한 공연 조회
+		case 3:
+			infoReservationList(reservation);
+			break;
+			
+		//캐시 충전	
+		case 4:
+			checkMoney();
+			break;
+			
+		case 0:
+			System.out.println("[안내] 이용해주셔서 감사합니다.");
+			mainMenu();
+			break;
+		
+		default:
+			System.out.println("[오류] 화면에 표시된 숫자만 입력해주세요.");
+			memberMenu(memberId);
+		}
+		
+	}
+	
+	public void checkMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void infoReservationList(ArrayList<Reservation> reservation) {
+	
+		printLine();
+		printSpace("\t<3. 예약 조회  >");
+		System.out.println();
+		System.out.println("* 뒤로가기 : '0' 입력*");
+		
+		int menuNo = inputNumber();
+		
+		for (int i = 0; i < reservation.size(); i++) {
+		    System.out.print(reservation.get(i) + " ");
+		}
+	
+		
+		if(menuNo == 0 ) {
+		
+		}else {
+			System.out.println("[오류] 뒤로가려면 '0'을 입력해주세요.");
+		}
+	}
+
+	public void infoReservation(String memberId) {
+	
+		printLine();
+		printSpace("\t< 2. 공연 예약  >");
+		System.out.println("* 관람하고 싶은 공연의 번호를 입력해주세요.*");
+		System.out.println("* 뒤로가기 : '0' 입력*");
+		System.out.println();
+		
+		//공연 정보 가져오기 
+		service.getPlayPrint();
+		
+		int menuNo = inputNumber();
+		
+		//while 문 조절
+		boolean loop = true;
+		
+		if(menuNo != 0) {
+			
+			boolean seatCheck = true;
+			
+				while(loop) {
+				// 공연 좌석 선택
+				printLine();
+				printSpace("\t< 2. 공연 좌석 선택>");
+				System.out.println();
+							
+				//공연 좌석 출력 
+				service.reservation(menuNo);
+							
+				System.out.println();
+				System.out.println("* 원하는 좌석 타입을 선택 해주세요.*");
+				System.out.println();
+				System.out.println("1 : VIP석, 2: S석, 3 : R석");
+				System.out.println();
+				
+							
+				int type = inputNumber();
+				
+				//원하는 좌석 출력함
+				seatCheck = service.userSeatChoice(type);
+				
+				if(seatCheck == true) {
+					// 사용자로부터 원하는 좌석 번호 입력 받게 함
+					System.out.println("* 원하는 좌석 번호를 입력해주세요. *");
+					int seatChoice = inputNumber();
+					
+					//좌석 예약
+					boolean result = service.userSeatChoice(menuNo, type, seatChoice, memberId);
+					
+					if(result) {
+						System.out.println("[안내] 좌석 예약을 완료했습니다. 즐거운 관람 되시길 바랍니다!");
+						System.out.println();
+						
+						loop = false;
+						memberMenu(memberId);
+						break;
+					}else {
+						System.out.println("[오류] 화면에 표시된 좌석 숫자를 입력해주세요.");
+						System.out.println();
+						continue;
+					}
+					
+				}else {
+						//좌석 번호 틀렸을 때 
+						continue;
+					}
+				}
+			}else if(menuNo == 0){
+					memberMenu(memberId);
+			}
+	
+		
+		
+		
+	}
+
+	/**
+	 *  공연 정보 조회 화면 
+	 * @param memberId 
+	 */
+	public  void infoCheck(String memberId) {
+		
+		printLine();
+		printSpace("\t < 공연 조회 >");
+		System.out.println("* 현재 진행되고 있는 공연들 입니다! *");
+		System.out.println("* 뒤로가기 : '0' 입력*");
+		System.out.println();
+
+	
+		service.getPlayPrint();
+		
+		int menuNo = inputNumber();
+		
+		if(menuNo == 0) {
+			memberMenu(memberId);
+		}else {
+			System.out.println("[안내] 뒤로가기를 하려면 '0'을 입력하셔야 합니다.");
+			infoCheck(memberId);
+		}
+		
+		System.out.println();
+	
+	}
+	public  void printLine() {
+		System.out.println("***********************");
+	}
+	
+	public  void printSpace(String sub) {
+		System.out.println("\t"+sub);
+	}
+	
+	/**
+	 * 문자열 입력 반환
+	 * @return 입력 문자열
+	 */
+	public String inputString() {
+		String data = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			data = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	/**
+	 * 숫자 입력 반환
+	 * @return 입력 정수형 숫자
+	 */
+	public int inputNumber() {
+		String data = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			data = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Integer.parseInt(data);
+	}
+
+	
+}
